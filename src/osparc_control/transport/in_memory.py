485d6ba6633e48fda6175bd2ad0cf0c0f4b4c1c6
Andrei Neagu <neagu@itis.swiss>
2022-03-11 15:46:01 +0100

    added in memory transport

---
+++ src/osparc_control/transport/in_memory.py
@@ -0,0 +1,29 @@
+from typing import Dict
+from queue import Queue
+
+from .interface import BaseTransport
+
+
+class InMemoryTransport(metaclass=BaseTransport):
+    """
+    Blocking in memory implementation, working with queues.
+    Can only be mixed with threading.
+
+    - sends data to `destination`
+    - fetches data from `source`
+    """
+
+    _SHARED_QUEUES: Dict[str, Queue] = {}
+
+    def __init__(self, source: str, destination: str):
+        self.source: str = source
+        self.destination: str = destination
+
+        self._SHARED_QUEUES[self.source] = Queue()
+        self._SHARED_QUEUES[self.destination] = Queue()
+
+    def send_bytes(self, payload: bytes) -> None:
+        self._SHARED_QUEUES[self.destination].put(payload)
+
+    def receive_bytes(self) -> bytes:
+        return self._SHARED_QUEUES[self.source].get()
---
+++ tests/test_transport_in_memory.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+from osparc_control.transport.in_memory import InMemoryTransport
+
+# UTILS
+
+
+def _payload_generator(start: int, stop: int) -> Iterable[bytes]:
+    assert start < stop
+    for k in range(start, stop):
+        yield f"test{k}".encode()
+
+
+# TESTS
+
+
+def test_send_receive():
+    sender = InMemoryTransport("A", "B")
+    receiver = InMemoryTransport("B", "A")
+
+    for message in _payload_generator(1, 10):
+        # TEST SENDER -> RECEIVER
+        sender.send_bytes(message)
+        assert receiver.receive_bytes() == message
+
+    for message in _payload_generator(11, 20):
+        # TEST RECEIVER -> SENDER
+        receiver.send_bytes(message)
+        assert sender.receive_bytes() == message
