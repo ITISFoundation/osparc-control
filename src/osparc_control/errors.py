e7db3e1ef58bffae1d375d8bc9bec6315f7c197d
Andrei Neagu <neagu@itis.swiss>
2022-03-16 11:16:00 +0100

    removed unused errors

--- src/osparc_control/errors.py
+++ src/osparc_control/errors.py
@@ -16,23 +16,3 @@ class CommnadNotAcceptedException(BaseControlException):
 
 class NoCommandReceivedArrivedException(BaseControlException):
     """Reply from remote host did not arrive in time"""
-
-
-class NotAllCommandManifestsHaveHandlersException(Exception):
-    def __init__(self, manifests_without_handler: List[CommandManifest]) -> None:
-        super().__init__(
-            (
-                "Not all commands define a handler. Please add one to the "
-                f"following {manifests_without_handler} to use this."
-            )
-        )
-
-
-class SignatureDoesNotMatchException(Exception):
-    def __init__(self, action: str, params: Dict[str, Any]) -> None:
-        super().__init__(
-            (
-                f"Handler signature did not match. Called for action="
-                f"'{action}' with params={params}"
-            )
-        )
