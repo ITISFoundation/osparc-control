6adc09c8425f30c18024e091621094ebbef28a96
Andrei Neagu <neagu@itis.swiss>
2022-03-16 10:05:17 +0100

    base working example

--- src/osparc_control/core.py
+++ src/osparc_control/core.py
@@ -2,7 +2,7 @@ from collections import deque
 from queue import Queue
 from threading import Thread
 from time import sleep
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union, List
 from uuid import getnode, uuid4
 
 from pydantic import ValidationError
@@ -12,8 +12,15 @@ from tenacity.wait import wait_fixed
 
 from osparc_control.transport.zeromq import ZeroMQTransport
 
-from .errors import NoReplyException
+from .errors import (
+    NoReplyException,
+    CommnadNotAcceptedException,
+    NoCommandReceivedArrivedException,
+    NotAllCommandManifestsHaveHandlersException,
+    SignatureDoesNotMatchException,
+)
 from .models import (
+    CommandReceived,
     CommandManifest,
     CommandReply,
     CommandRequest,
@@ -23,8 +30,16 @@ from .models import (
 )
 from .transport.base_transport import SenderReceiverPair
 
+
+_MINUTE: float = 60.0
+
 WAIT_FOR_MESSAGES: float = 0.01
 WAIT_BETWEEN_CHECKS: float = 0.1
+# NOTE: this effectively limits the time between when
+# the two remote sides can start to communicate
+WAIT_FOR_RECEIVED: float = 1 * _MINUTE
+
+DEFAULT_LISTEN_PORT: int = 7426
 
 
 def _generate_request_id() -> str:
@@ -46,22 +61,45 @@ def _get_sender_receiver_pair(
 
 class SomeEntryPoint:
     def __init__(
-        self, remote_host: str, remote_port: int = 7426, listen_port: int = 7426
+        self,
+        remote_host: str,
+        exposed_interface: List[CommandManifest],
+        remote_port: int = DEFAULT_LISTEN_PORT,
+        listen_port: int = DEFAULT_LISTEN_PORT,
     ) -> None:
 
         self._sender_receiver_pair: SenderReceiverPair = _get_sender_receiver_pair(
             remote_host=remote_host, remote_port=remote_port, listen_port=listen_port
         )
 
+        def _process_manifest(manifest: CommandManifest) -> CommandManifest:
+            manifest._remapped_params = {x.name: x for x in manifest.params}
+            return manifest
+
+        # map by action name for
+        self._exposed_interface: Dict[str, CommandManifest] = {
+            x.action: _process_manifest(x) for x in exposed_interface
+        }
+        if len(self._exposed_interface) != len(exposed_interface):
+            raise ValueError(
+                (
+                    f"Provided exposed_interface={exposed_interface} "
+                    "contains CommandManifest with same action name."
+                )
+            )
+
         self._request_tracker: RequestsTracker = {}
-        # TODO: below must be protected by a lock
+        # NOTE: deque is thread safe only when used with appends and pops
         self._incoming_request_tracker = deque()
 
         self._out_queue: Queue = Queue()
+        self._incoming_command_queue: Queue = Queue()
 
-        # sending and receving threads
-        self._sender_thread: Thread = Thread(target=self._sender_worker)
-        self._receiver_thread: Thread = Thread(target=self._receiver_worker)
+        # sending and receiving threads
+        self._sender_thread: Thread = Thread(target=self._sender_worker, daemon=True)
+        self._receiver_thread: Thread = Thread(
+            target=self._receiver_worker, daemon=True
+        )
         self._continue: bool = True
 
     def _sender_worker(self) -> None:
@@ -80,6 +118,74 @@ class SomeEntryPoint:
 
         self._sender_receiver_pair.sender_cleanup()
 
+    def _handle_command_request(self, response: bytes) -> None:
+        command_request: Optional[CommandRequest] = CommandRequest.from_bytes(response)
+        assert command_request
+
+        def _refuse_and_return(error_message: str) -> None:
+            self._out_queue.put(
+                CommandReceived(
+                    request_id=command_request.request_id,
+                    accepted=False,
+                    error_message=error_message,
+                )
+            )
+            return
+
+        # check if command exists
+        if command_request.action not in self._exposed_interface:
+            error_message = (
+                f"No registered command found for action={command_request.action}. "
+                f"Supported actions {list(self._exposed_interface.keys())}"
+            )
+            _refuse_and_return(error_message)
+
+        manifest = self._exposed_interface[command_request.action]
+
+        # check command_type matches the one declared in the manifest
+        if command_request.command_type != manifest.command_type:
+            error_message = (
+                f"Incoming request command_type {command_request.command_type} do not match "
+                f"manifest's command_type {manifest.command_type}"
+            )
+            _refuse_and_return(error_message)
+
+        # check if provided parametes match manifest
+        incoming_params_set = set(command_request.params.keys())
+        manifest_params_set = set(manifest._remapped_params.keys())
+        if incoming_params_set != manifest_params_set:
+            error_message = (
+                f"Incoming request params {command_request.params} do not match "
+                f"manifest's params {manifest.params}"
+            )
+            _refuse_and_return(error_message)
+
+        # accept command
+        self._out_queue.put(
+            CommandReceived(
+                request_id=command_request.request_id,
+                accepted=True,
+                error_message=None,
+            )
+        )
+
+        self._incoming_request_tracker.append(command_request)
+
+    def _handle_command_reply(self, response: bytes) -> None:
+        command_reply: Optional[CommandReply] = CommandReply.from_bytes(response)
+        assert command_reply
+
+        tracked_request: TrackedRequest = self._request_tracker[command_reply.reply_id]
+        tracked_request.reply = command_reply
+
+    def _handle_command_received(self, response: bytes) -> None:
+        command_received: Optional[CommandReceived] = CommandReceived.from_bytes(
+            response
+        )
+        assert command_received
+
+        self._incoming_command_queue.put_nowait(command_received)
+
     def _receiver_worker(self) -> None:
         self._sender_receiver_pair.receiver_init()
 
@@ -93,22 +199,21 @@ class SomeEntryPoint:
             # NOTE: pydantic does not support polymorphism
             # SEE https://github.com/samuelcolvin/pydantic/issues/503
 
-            # check if is CommandRequest
+            # case CommandReceived
             try:
-                command_request = CommandRequest.from_bytes(response)
-                assert command_request
-                self._incoming_request_tracker.append(command_request)
+                self._handle_command_received(response)
             except ValidationError:
                 pass
 
-            # check if is CommandReply
+            # case CommandRequest
             try:
-                command_reply = CommandReply.from_bytes(response)
-                assert command_reply
-                tracked_request: TrackedRequest = self._request_tracker[
-                    command_reply.reply_id
-                ]
-                tracked_request.reply = command_reply
+                self._handle_command_request(response)
+            except ValidationError:
+                pass
+
+            # case CommandReply
+            try:
+                self._handle_command_reply(response)
             except ValidationError:
                 pass
 
@@ -118,25 +223,39 @@ class SomeEntryPoint:
 
     def _enqueue_call(
         self,
-        manifest: CommandManifest,
+        action: str,
         params: Optional[Dict[str, Any]],
         expected_command_type: CommnadType,
     ) -> CommandRequest:
         """validates and enqueues the call for delivery to remote"""
-        request = CommandRequest.from_manifest(
-            manifest=manifest, request_id=_generate_request_id(), params=params
+        request = CommandRequest(
+            request_id=_generate_request_id(),
+            action=action,
+            params={} if params is None else params,
+            command_type=expected_command_type,
         )
-
-        if request.command_type != expected_command_type:
-            raise RuntimeError(
-                f"Request {request} was expected to have command_type={expected_command_type}"
-            )
 
         self._request_tracker[request.request_id] = TrackedRequest(
             request=request, reply=None
         )
 
         self._out_queue.put(request)
+
+        # check command_received status
+        try:
+            for attempt in Retrying(
+                stop=stop_after_delay(WAIT_FOR_RECEIVED),
+                wait=wait_fixed(WAIT_BETWEEN_CHECKS),
+            ):
+                with attempt:
+                    command_received = self._incoming_command_queue.get(block=False)
+
+                    if not command_received.accepted:
+                        raise CommnadNotAcceptedException(
+                            command_received.error_message
+                        )
+        except RetryError:
+            raise NoCommandReceivedArrivedException()
 
         return request
 
@@ -157,19 +276,19 @@ class SomeEntryPoint:
         self._receiver_thread.join()
 
     def request_and_forget(
-        self, manifest: CommandManifest, params: Optional[Dict[str, Any]] = None
+        self, action: str, params: Optional[Dict[str, Any]] = None
     ) -> None:
         """No reply will be provided by remote side for this command"""
-        self._enqueue_call(manifest, params, CommnadType.WITHOUT_REPLY)
+        self._enqueue_call(action, params, CommnadType.WITHOUT_REPLY)
 
     def request_and_check(
-        self, manifest: CommandManifest, params: Optional[Dict[str, Any]] = None
+        self, action: str, params: Optional[Dict[str, Any]] = None
     ) -> str:
         """
         returns a `request_id` to be used with `check_for_reply` to monitor
         if a reply to the request was returned.
         """
-        request = self._enqueue_call(manifest, params, CommnadType.WITH_REPLAY)
+        request = self._enqueue_call(action, params, CommnadType.WITH_REPLAY)
         return request.request_id
 
     def check_for_reply(self, request_id: str) -> Tuple[bool, Optional[Any]]:
@@ -204,7 +323,7 @@ class SomeEntryPoint:
 
     def request_and_wait(
         self,
-        manifest: CommandManifest,
+        action: str,
         timeout: float,
         params: Optional[Dict[str, Any]] = None,
     ) -> Optional[Any]:
@@ -213,7 +332,7 @@ class SomeEntryPoint:
         A timeout for this function is required. If the timeout is reached `None` will
         be returned.
         """
-        request = self._enqueue_call(manifest, params, CommnadType.WAIT_FOR_REPLY)
+        request = self._enqueue_call(action, params, CommnadType.WAIT_FOR_REPLY)
 
         try:
             for attempt in Retrying(
@@ -228,13 +347,57 @@ class SomeEntryPoint:
         except RetryError:
             return None
 
-    def get_incoming_request(self) -> Optional[CommandRequest]:
-        """will try to fetch an incoming request, returns None if nothing is present"""
+    def get_incoming_requests(self) -> List[CommandRequest]:
+        """retruns all accumulated CommandRequests"""
+        results = deque()
+
+        # fetch all elements empty
+        # below implementation is thread-safe
         try:
-            return self._incoming_request_tracker.pop()
+            while True:
+                results.append(self._incoming_request_tracker.pop())
         except IndexError:
-            return None
+            pass
+
+        return list(results)
 
     def reply_to_command(self, request_id: str, payload: Any) -> None:
         """provide the reply back to a command"""
         self._out_queue.put(CommandReply(reply_id=request_id, payload=payload))
+
+    def process_incoming_requests(self, keep_processing: bool = False) -> None:
+        """
+        Requires the user to define handler on all CommandManifest entries
+        param: `keep_processing` if True will process in an infinite loop without exiting
+        """
+        all_commands_define_a_handler = all(
+            manifest.handler is not None
+            for manifest in self._exposed_interface.values()
+        )
+        if not all_commands_define_a_handler:
+            manifests_without_handler = [
+                x for x in self._exposed_interface.values() if x.handler is None
+            ]
+            raise NotAllCommandManifestsHaveHandlersException(manifests_without_handler)
+
+        can_continue = True
+        while can_continue:
+            for incoming_request in self.get_incoming_requests():
+                # fetch manifest
+                manifest = self._exposed_interface[incoming_request.action]
+
+                # call handler with params and get the result
+                assert manifest.handler
+                try:
+                    result = manifest.handler(**incoming_request.params)
+                except TypeError as e:
+                    raise SignatureDoesNotMatchException(
+                        manifest.action, incoming_request.params
+                    ) from e
+
+                # reply to command
+                self.reply_to_command(
+                    request_id=incoming_request.request_id, payload=result
+                )
+
+            can_continue = keep_processing
--- src/osparc_control/errors.py
+++ src/osparc_control/errors.py
@@ -1,6 +1,38 @@
+from typing import Any, Dict, List
+from osparc_control.models import CommandManifest
+
+
 class BaseControlException(Exception):
     """inherited by all exceptions in this moduls"""
 
 
 class NoReplyException(BaseControlException):
     """Used when retrying for a result"""
+
+
+class CommnadNotAcceptedException(BaseControlException):
+    """Command was not accepted by remote"""
+
+
+class NoCommandReceivedArrivedException(BaseControlException):
+    """Reply from remote host did not arrive in time"""
+
+
+class NotAllCommandManifestsHaveHandlersException(Exception):
+    def __init__(self, manifests_without_handler: List[CommandManifest]) -> None:
+        super().__init__(
+            (
+                "Not all commands define a handler. Please add one to the "
+                f"following {manifests_without_handler} to use this."
+            )
+        )
+
+
+class SignatureDoesNotMatchException(Exception):
+    def __init__(self, action: str, params: Dict[str, Any]) -> None:
+        super().__init__(
+            (
+                f"Handler signature did not match. Called for action="
+                f"'{action}' with params={params}"
+            )
+        )
