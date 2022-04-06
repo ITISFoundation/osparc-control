3e5d80ee465ef34c5c08a50e40d5fdef1d12fd6d
Andrei Neagu <neagu@itis.swiss>
2022-04-06 16:46:24 +0200

    added extra test

--- examples/1_simple/requester.py
+++ examples/1_simple/requester.py
@@ -12,4 +12,5 @@ with paired_transmitter:
         "random_in_range", timeout=10.0, params={"a": 1, "b": 10}
     )
     print(random_int)
+    assert random_int is not None  # noqa: S101
     assert 1 <= random_int <= 10  # noqa: S101
