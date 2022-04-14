d7f6077b6429223565f05feef613428396eeab69
Andrei Neagu <neagu@itis.swiss>
2022-04-14 07:11:59 +0200

    more clean requirments installation

--- tests/examples/test_all.py
+++ tests/examples/test_all.py
@@ -37,7 +37,26 @@ def example_3_tsolver(examples_path: Path) -> Path:
     return path
 
 
+@pytest.fixture
+def example_4_coupled_solver(examples_path: Path) -> Path:
+    path = (examples_path / "4_coupled_solver").resolve()
+    assert path.exists()
+    return path
+
+
 # UTILS
+
+
+def install_dependencies(search_path: Path) -> None:
+    requirements_txt = search_path / "requirements.txt"
+    if not (requirements_txt.exists() and requirements_txt.is_file()):
+        print(f"No requirements found in {search_path.name}")
+        return
+
+    print(f"Installing dependencies for {search_path.name}:")
+    for dependency in requirements_txt.read_text().split("\n"):
+        print(f"- '{dependency}'")
+        pip.main(["install", dependency])
 
 
 def assert_run_in_parallel(scrips_to_run: List[Path]) -> None:
@@ -70,11 +89,6 @@ def assert_run_in_parallel(scrips_to_run: List[Path]) -> None:
             assert process.returncode == 0, stdout.decode()
 
 
-@pytest.fixture
-def install_matplotlib() -> None:
-    pip.main(["install", "matplotlib"])
-
-
 # TESTS
 
 
@@ -92,10 +106,19 @@ def test_example_2_base_time_add_runs(example_2_base_time_add_path: Path) -> Non
     assert_run_in_parallel([controller_path, solver_path])
 
 
-def test_example_example_3_tsolver(
-    example_3_tsolver: Path, install_matplotlib: None
-) -> None:
-    controller_path = example_3_tsolver / "Tsolver.py"
-    solver_path = example_3_tsolver / "Controller.py"
+def test_example_example_3_tsolver(example_3_tsolver: Path) -> None:
+    install_dependencies(example_3_tsolver)
 
-    assert_run_in_parallel([controller_path, solver_path])
+    t_solver_path = example_3_tsolver / "Tsolver.py"
+    controller_path = example_3_tsolver / "Controller.py"
+
+    assert_run_in_parallel([controller_path, t_solver_path])
+
+
+def test_4_coupled_solver(example_4_coupled_solver: Path) -> None:
+    install_dependencies(example_4_coupled_solver)
+
+    coupler_script_path = example_4_coupled_solver / "coupler_script.py"
+    tsolver_script_path = example_4_coupled_solver / "tsolver_script.py"
+
+    assert_run_in_parallel([coupler_script_path, tsolver_script_path])
